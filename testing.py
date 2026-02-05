5c7cbb115d875b47878ef20bac53cbb009663698
lianakim12 <kqm9bw@virginia.edu>
2026-02-05 14:04:08 +0000

    removed unnecessary files

--- testing.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd  # For data manipulation and analysis
-college_df = pd.read_csv('/workspaces/ml_lab_week_3/data/cc_institution_details.csv')
-job_df = pd.read_csv('/workspaces/ml_lab_week_3/data/Placement_Data_Full_Class.csv')
-print(college_df.info())
